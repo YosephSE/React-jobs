f4a465ba0b34354f7077f2bbae59fb5f46628160
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-18 00:50:01 +0300

    all jobs added

---
+++ src/components/Job.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+const Job = ({id ,type, title, desc, salary, location}) => {
+  return (
+    <div className="bg-white rounded-xl shadow-md relative">
+          <div className="p-4">
+            <div className="mb-6">
+              <div className="text-gray-600 my-2">{type}</div>
+              <h3 className="text-xl font-bold">{title}</h3>
+            </div>
+
+            <div className="mb-5">{desc}</div>
+
+            <h3 className="text-indigo-500 mb-2">{salary}</h3>
+
+            <div className="border border-gray-100 mb-5"></div>
+
+            <div className="flex flex-col lg:flex-row justify-between mb-4">
+              <div className="text-orange-700 mb-3">
+                <i className="fa-solid fa-location-dot text-lg"></i>
+                {location}
+              </div>
+              <a
+                href={`/job/${id}`}
+                className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+              >
+               Read More
+              </a>
+            </div>
+          </div>
+        </div>  
+  )
+}
+
+export default Job
--- src/components/jobListings.jsx
+++ src/components/jobListings.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Job from './Job'
 import jobs from '../../jobs.json'
 
-
 const JobListings = () => {
-    const jobLists = jobs.jobs
-    console.log(jobLists)
+  const jobsList = jobs.jobs.slice(0,3)
+  console.log(jobsList)
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -12,94 +12,10 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div className="bg-white rounded-xl shadow-md relative">
-            <div className="p-4">
-              <div className="mb-6">
-                <div className="text-gray-600 my-2">Full-Time</div>
-                <h3 className="text-xl font-bold">Senior React Developer</h3>
-              </div>
-
-              <div className="mb-5">
-               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
-              </div>
-
-              <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
-
-              <div className="border border-gray-100 mb-5"></div>
-
-              <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
-                  Boston, MA
-                </div>
-                <a
-                  href="job.html"
-                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                >
-                 Read More
-                </a>
-              </div>
-            </div>
-          </div>
-        
-          <div className="bg-white rounded-xl shadow-md relative">
-            <div className="p-4">
-              <div className="mb-6">
-                <div className="text-gray-600 my-2">Remote</div>
-                <h3 className="text-xl font-bold">Front-End Engineer (React)</h3>
-              </div>
-
-              <div className="mb-5">
-               Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
-              </div>
-
-              <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-              <div className="border border-gray-100 mb-5"></div>
-
-              <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
-                  Miami, FL
-                </div>
-                <a
-                  href="job.html"
-                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                >
-                 Read More
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="bg-white rounded-xl shadow-md relative">
-            <div className="p-4">
-              <div className="mb-6">
-                <div className="text-gray-600 my-2">Remote</div>
-                <h3 className="text-xl font-bold">React.js Developer</h3>
-              </div>
-
-              <div className="mb-5">
-                Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
-              </div>
-
-              <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-              <div className="border border-gray-100 mb-5"></div>
-
-              <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
-                  Brooklyn, NY
-                </div>
-                <a
-                  href="job.html"
-                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                >
-                 Read More
-                </a>
-              </div>
-            </div>
-          </div>
+        { jobsList.map((job) => (
+          <Job id={job.id} key={job.id} type={job.type}  title={job.title} desc={job.description} location={job.location} salary={job.salary} />
+        ))}
+          
         </div>
       </div>
     </section>
