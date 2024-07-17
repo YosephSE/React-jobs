53de9934390e1199728b914463ac80506680507b
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-17 13:29:23 +0300

    everything else

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <>
+    <NavBar/>
+    <Hero/>
+    </>
+  )
+}
+
+export default App
