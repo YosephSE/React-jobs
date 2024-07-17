c1378311f2c79377b449ee25bc58e323c9079342
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-17 13:28:19 +0300

    Hero section added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='py-20 flex flex-col items-center justify-center bg-indigo-700 text-white'>
+      <div className="text-5xl font-extrabold text-center px-3">Become a React Dev</div>
+      <div className="text-xl text-center px-3">Find the React job that fits your skills and needs</div>
+      
+    </div>
+  )
+}
+
+export default Hero
