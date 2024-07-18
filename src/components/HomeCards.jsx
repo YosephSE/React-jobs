982658b07f46e7baa47646e022a95c530c251672
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-18 23:35:09 +0300

    links updated

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const HomeCards = () => {
   return (
     <section className="py-4">
@@ -10,24 +10,24 @@ const HomeCards = () => {
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
             </p>
-            <a
-              href="/jobs.html"
+            <Link
+              to="/jobs"
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
               Browse Jobs
-            </a>
+            </Link>
           </div>
           <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
-            <a
-              href="/add-job.html"
+            <Link
+              to="/add"
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
               Add Job
-            </a>
+            </Link>
           </div>
         </div>
       </div>
